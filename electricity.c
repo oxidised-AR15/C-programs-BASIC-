857a8698b3dd5463ebdd533c86650941db8e33c1
oxidised-AR15 <166270877+oxidised-AR15@users.noreply.github.com>
2025-09-13 12:33:25 +0530

    Added electricity bill and quadratic roots programs

---
+++ electricity.c
@@ -0,0 +1,29 @@
+// From 0 to 100 units bill is free but have to give confirmed charge of 300 Rs and above it 300 fixed charge plus per units charge after usage of 100 units according to slab will be your total bill.
+
+#include <stdio.h>
+int main() {
+    int units,u,charge=300;
+    float bill;
+    printf("Enter electricity units consumed :");
+    scanf("%d", &units);
+    if (units<=100) {
+        bill=charge;    
+        printf("Bill is :Rs %.2f",bill);
+    }
+    else if ((units>100)&&(units<=200)) {
+        u=units-100;
+        bill=(u*0.50)+300;   
+        printf("Bill is :Rs %.2f",bill);
+    }
+    else if ((units>200)&&(units<=300)) {
+        u=units-100;
+        bill=(u*0.80)+300;
+        printf("Bill is :Rs %.2f Rs",bill);
+    }
+    else {
+        u=units-100;
+        bill=u+300;
+        printf("Bill is :Rs %.2f Rs",bill);
+    }
+    return 0;
+}
---
+++ roots.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+        int a,b,c;
+        float d,r1,r2;
+        printf("Enter a (coefficient of x^2):");
+        scanf("%d",&a);
+        printf("Enter b (coefficient) of x :");
+        scanf("%d",&b);
+        printf("Enter c :");
+        scanf("%d",&c);
+        d=((b*b)-(4*a*c));
+        r1=((-b)+(sqrt(d)))/(2*a);
+        r2=((-b)-(sqrt(d)))/(2*a);
+        if (d<0) {
+            printf("No real roots possible , Its complex number\n");
+        }
+        else if (d==0) {
+            printf("Two real roots\n");
+            printf("1st Root is : %.2f\n2nd Root is : %.2f\n",r1,r2);
+        }
+        else if (d>0) {
+            printf("Two Distinct roots\n");
+            printf("1st Root is : %.3f\n2nd Root is : %.2f\n",r1,r2);
+        }
+        return 0;    
+    }
